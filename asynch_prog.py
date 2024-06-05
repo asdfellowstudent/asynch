a21788bb30b5e6ed1dcfbf31a0e52074841438cf
Asadbek <pacankarate416@gmail.com>
2024-06-05 03:43:42 -0700

    hello

---
+++ asynch_prog.py
@@ -0,0 +1,21 @@
+import asyncio
+
+async def boil_water():
+    print("Water start boiling..")
+    await asyncio.sleep(3)
+    print("Water end boiling..")
+
+async def cut_onion():
+    print("Start cutting..")
+    await asyncio.sleep(2)
+    print("End cutting..")
+
+async def prepare_salad():
+    print("Start salad..")
+    await asyncio.sleep(4)
+    print("End salad..")
+
+async def main():
+    await asyncio.gather(boil_water(), cut_onion(), prepare_salad())
+
+asyncio.run()
