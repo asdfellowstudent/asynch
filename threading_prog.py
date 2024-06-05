d75652b4dbdfc4ef1656d67dfd71768555b0a411
Asadbek <pacankarate416@gmail.com>
2024-06-05 03:43:52 -0700

    hello2

---
+++ threading_prog.py
@@ -0,0 +1,14 @@
+# import threading
+# import requests
+# from bs4 import BeautifulSoup
+
+# def get_title(url, result, index):
+#     response = requests.get(url)
+#     soup = BeautifulSoup(response.content, 'html.parser')
+#     title = soup.title.string
+#     result[index] = title
+
+# urls = []
+# threads = []
+# for i in range(len(urls)):
+#     threads    
